1dd72faf361e2dee2b5453afbd47869a18affd3a
Fridolin Pokorny <fridolin@redhat.com>
2018-12-10 20:32:47 +0100

    Make cleanup job more generic

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # thoth-cleanup-job
 # Copyright(C) 2018 Fridolin Pokorny
 #
@@ -16,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""Delete old analyzers in the cluster."""
+"""Delete old resources in the cluster."""
 
 
 import datetime
@@ -25,199 +24,165 @@ import os
 import sys
 
 from dateutil.parser import parse as datetime_parser
-from pytimeparse.timeparse import timeparse
-import requests
+import click
+from pytimeparse import parse as parse_ttl
+from kubernetes import client
+from kubernetes import config
+from openshift.dynamic import DynamicClient
+from prometheus_client import CollectorRegistry
+from prometheus_client import Gauge
+from prometheus_client import Counter
+from prometheus_client import push_to_gateway
 
-from thoth.common import init_logging, get_service_account_token, OpenShift
+from thoth.common import init_logging
 from thoth.common import __version__ as __common__version__
-from thoth.common.exceptions import NotFoundException
+
 
 __version__ = f"0.6.2+common.{__common__version__}"
 
 
 init_logging()
-prometheus_registry = CollectorRegistry()
+
 
 _LOGGER = logging.getLogger("thoth.cleanup_job")
-_OPENSHIFT = OpenShift()
-
-
-KUBERNETES_API_URL = os.getenv(
-    "KUBERNETES_API_URL", "https://kubernetes.default.svc.cluster.local"
-)
-KUBERNETES_API_TOKEN = os.getenv("KUBERNETES_API_TOKEN") or get_service_account_token()
-KUBERNETES_VERIFY_TLS = bool(int(os.getenv("KUBERNETES_VERIFY_TLS", 0)))
-THOTH_MIDDLETIER_NAMESPACE = os.environ["THOTH_MIDDLETIER_NAMESPACE"]
-THOTH_CLEANUP_TIMEOUT = timeparse(os.getenv("THOTH_CLEANUP_TIMEOUT", "5d"))
-THOTH_METRICS_PUSHGATEWAY_URL = os.getenv("THOTH_METRICS_PUSHGATEWAY_URL")
-THOTH_MY_NAMESPACE = os.getenv("NAMESPACE", "thoth-test-core")
-
+_DEFAULT_TTL = parse_ttl(os.getenv("THOTH_CLEANUP_DEFAULT_TTL") or "7d")
+_CLEANUP_LABEL_SELECTOR = "mark=cleanup"
+_PROMETHEUS_REGISTRY = CollectorRegistry()
+_THOTH_METRICS_PUSHGATEWAY_URL = os.getenv("THOTH_METRICS_PUSHGATEWAY_URL")
 _METRIC_RUNTIME = Gauge(
-    "thoth_cleanup_job_runtime_seconds",
-    "Runtime of cleanup job in seconds.",
-    [],
-    registry=prometheus_registry,
+    "thoth_cleanup_job_runtime_seconds", "Runtime of cleanup job in seconds.", [], registry=_PROMETHEUS_REGISTRY
 )
-_METRIC_SOLVER_JOBS = Counter(
-    "thoth_cleanup_job_solver_jobs",
-    "Solver Jobs cleaned up.",
-    ["env", "op"],
-    registry=prometheus_registry,
+_METRIC_JOBS = Counter(
+    "thoth_cleanup_jobs", "Jobs cleaned up.", ["env", "op"], registry=_PROMETHEUS_REGISTRY
+)
+_METRIC_BUILDCONFIGS = Counter(
+    "thoth_cleanup_buildconfigs", "Buildconfigs cleaned up.", ["env", "op"], registry=_PROMETHEUS_REGISTRY
+)
+_METRIC_IMAGESTREAMS = Counter(
+    "thoth_cleanup_imagestreams", "Imagestreams cleaned up.", ["env", "op"], registry=_PROMETHEUS_REGISTRY
+)
+_METRIC_CONFIGMAPS = Counter(
+    "thoth_cleanup_configmaps", "Configmaps cleaned up.", ["env", "op"], registry=_PROMETHEUS_REGISTRY
+)
+_RESOURCES = frozenset(
+    (
+        # apiVersion, Type, delete based on creation (if false, take completionTime in status), metric to report to
+        ("build.openshift.io/v1", "BuildConfig", True, _METRIC_BUILDCONFIGS),
+        ("image.openshift.io/v1", "ImageStream", True, _METRIC_IMAGESTREAMS),
+        ("v1", "ConfigMap", True, _METRIC_CONFIGMAPS),
+        ("batch/v1", "Job", False, _METRIC_JOBS),
+    )
 )
 
-# Metrics Exporter Metrics
-_METRIC_INFO = Gauge(
-    "thoth_cleanup_job_info",
-    "Thoth Cleanup Job information",
-    ["env", "version"],
-    registry=prometheus_registry,
-)
-_METRIC_INFO.labels(THOTH_MY_NAMESPACE, __version__).inc()
 
+def _do_cleanup(cleanup_namespace: str) -> None:
+    """Perform the actual cleanup."""
+    config.load_incluster_config()
+    dyn_client = DynamicClient(client.ApiClient(configuration=client.Configuration()))
+    now = datetime.datetime.now(datetime.timezone.utc)
 
-def _get_pods(label_selector: str) -> dict:
-    """Get currently running analyzers by label."""
-    # TODO: pagination?
-    endpoint = (
-        f"{KUBERNETES_API_URL}/api/v1/namespaces/{THOTH_MIDDLETIER_NAMESPACE}/"
-        f"pods?labelSelector={label_selector}"
-    )
+    for resource_version, resource_type, creation_delete, metric in _RESOURCES:
+        resources = dyn_client.resources.get(
+            api_version=resource_version, kind=resource_type
+        )
+        for item in resources.get(label_selector=_CLEANUP_LABEL_SELECTOR, namespace=cleanup_namespace).items:
+            _LOGGER.debug(
+                "Checking expiration of resource %r from namespace %r of kind %r",
+                item.metadata.name,
+                cleanup_namespace,
+                resources.kind
+            )
 
-    response = requests.get(
-        endpoint,
-        verify=KUBERNETES_VERIFY_TLS,
-        headers={
-            "Authorization": "Bearer {}".format(KUBERNETES_API_TOKEN),
-            "Content-Type": "application/json",
-        },
-    )
-    response.raise_for_status()
-    _LOGGER.debug("Full response from Kubernetes master is: %r", response.json())
-
-    pods = response.json().get("items", [])
-    _LOGGER.info(
-        "Kubernetes master returned %r pods with label %r", len(pods), label_selector
-    )
-
-    return pods
-
-
-def _delete_pod(pod_name):
-    endpoint = "{}/{}/{}/{}".format(
-        KUBERNETES_API_URL,
-        "api/v1/namespaces",
-        THOTH_MIDDLETIER_NAMESPACE,
-        "pods",
-        pod_name,
-    )
-    response = requests.delete(
-        endpoint,
-        verify=KUBERNETES_VERIFY_TLS,
-        headers={
-            "Authorization": "Bearer {}".format(KUBERNETES_API_TOKEN),
-            "Content-Type": "application/json",
-        },
-    )
-    response.raise_for_status()
-    _LOGGER.debug("Response from Kubernetes master on deletion is: %r", response.json())
-
-    return (pod for pod in response.json()["items"])
-
-
-def _delete_old_analyzes(analyzers):
-    """Delete old analyzers."""
-    now = datetime.datetime.now().timestamp()
-    lifetime = datetime.timedelta(seconds=THOTH_ANALYZER_CLEANUP_TIME).total_seconds()
-
-    for analyzer in analyzers:
-        # TODO: also delete pods where pull failed
-        creation_time = datetime_parser(
-            pod["metadata"]["creationTimestamp"]
-        ).timestamp()
-        if creation_time + lifetime <= now:
-            _LOGGER.info("Deleting pod %r", pod["metadata"]["name"])
+            ttl = item.metadata.ttl
             try:
-                _delete_pod(pod["metadata"]["name"])
-            except Exception:
+                parsed_ttl = parse_ttl(ttl) if ttl else _DEFAULT_TTL
+            except Exception as exc:
                 _LOGGER.exception(
-                    "Failed to delete pod {!r}, error is not fatal".format(
-                        pod["metadata"]["name"]
-                    )
+                    "Failed to parse TTL %r for resource %r of type %r in namespace %r the object will not be deleted",
+                    ttl,
+                    item.metadata.name,
+                    resources.kind,
+                    cleanup_namespace
                 )
-        else:
-            _LOGGER.info("Keeping pod %r, not too old yet", pod["metadata"]["name"])
+                continue
 
-
-def _delete_object(api: str, kind: str, name: str):
-    """Delete Object of Kind kind within API Group api."""
-    endpoint = f"{KUBERNETES_API_URL}/{api}/namespaces/{THOTH_MIDDLETIER_NAMESPACE}/{kind}/{name}"
-
-    response = requests.delete(
-        endpoint,
-        verify=KUBERNETES_VERIFY_TLS,
-        headers={
-            "Authorization": "Bearer {}".format(KUBERNETES_API_TOKEN),
-            "Content-Type": "application/json",
-        },
-    )
-    response.raise_for_status()
-    _LOGGER.debug("Response from Kubernetes is: %r", response.json())
-
-
-def _delete_outdated_jobs(jobs: dict):
-    """Delete succeeded and outdated Jobs."""
-    now = datetime.datetime.now().timestamp()
-    lifetime = datetime.timedelta(seconds=THOTH_CLEANUP_TIMEOUT).total_seconds()
-
-    for ding in jobs:
-        creation_time = datetime_parser(
-            ding["metadata"]["creationTimestamp"]
-        ).timestamp()
-
-        if creation_time + lifetime <= now:
-            _LOGGER.debug("deleting Job %r", ding["metadata"]["name"])
-            try:
-                _delete_object("apis/batch/v1", "jobs", ding["metadata"]["name"])
-            except Exception:
-                _LOGGER.exception(
-                    "Failed to delete Job {!r}, error is not fatal".format(
-                        ding["metadata"]["name"]
+            if creation_delete:
+                if not item.metadata.creationTimestamp:
+                    _LOGGER.info(
+                        "Skipping resource %r of type %r- no creation timestsamp found in metadata",
+                        item.metadata.name,
+                        resources.type
                     )
+                    continue
+                created_str = item.metadata.creationTimestamp
+            else:
+                if not item.status.completionTime:
+                    _LOGGER.info(
+                        "Skipping resource %r of type %r- no completion time found in status field",
+                        item.metadata.name,
+                        resources.kind
+                    )
+                    continue
+                created_str = item.status.completionTime
+
+            created = datetime_parser(created_str)
+            lived_for = (now - created).total_seconds()
+
+            if lived_for > parsed_ttl:
+                _LOGGER.info(
+                    "Deleting resource %r of type %r in namespace %r - created at %r",
+                    item.metadata.name,
+                    resources.kind,
+                    cleanup_namespace,
+                    created_str
+                )
+                try:
+                    resources.delete(name=item.metadata.name, namespace=cleanup_namespace)
+                    metric.inc()
+                except Exception as exc:
+                    _LOGGER.exception(
+                        "Failed to delete resource %r of type %r in namespace %r",
+                        item.metadata.name,
+                        resources.kind,
+                        cleanup_namespace
+                    )
+            else:
+                _LOGGER.info(
+                    "Keeping resource %r of type %r in namespace %r ttl not expired yet",
+                    item.metadata.name,
+                    resources.kind,
+                    cleanup_namespace
                 )
 
-            _METRIC_SOLVER_JOBS.labels(THOTH_MY_NAMESPACE, "delete").inc()
-        else:
-            _LOGGER.debug("keeping Job %r, it's to young", ding["metadata"]["name"])
-            _METRIC_SOLVER_JOBS.labels(THOTH_MY_NAMESPACE, "noop").inc()
 
+@click.command()
+@click.option("--verbose", is_flag=True, envvar="THOTH_CLEANUP_VERBOSE", help="Be verbose about what is going on.")
+@click.option(
+    "--cleanup-namespace",
+    type=str,
+    required=True,
+    envvar="THOTH_CLEANUP_NAMESPACE",
+    help="Namespace in which cleanups should be done.",
+)
+def cli(cleanup_namespace: str, verbose: bool = False):
+    """Operator handling Thoth's workloads."""
+    if verbose:
+        _LOGGER.setLevel(logging.DEBUG)
 
-if __name__ == "__main__":
-    _LOGGER.setLevel(
-        logging.DEBUG if bool(int(os.getenv("CLEANUP_DEBUG", 0))) else logging.INFO
-    )
-
-    _LOGGER.info(f"Thoth Cleanup Job v{__version__} starting...")
-    _LOGGER.debug("running with DEBUG log level")
+    _LOGGER.info("Thoth Cleanup Job v%s starting...", __version__)
+    _LOGGER.info("Cleanup will be performed in namespace %r", cleanup_namespace)
 
     with _METRIC_RUNTIME.time():
         try:
-            # let's delete all succeeded and outdated Solver Jobs
-            jobs = get_jobs("component%3Dsolver-f27", "thoth-test-core")
+            _do_cleanup(cleanup_namespace)
+        finally:
+            if _THOTH_METRICS_PUSHGATEWAY_URL:
+                try:
+                    _LOGGER.info("Submitting metrics to Prometheus pushgateway %r", THOTH_METRICS_PUSHGATEWAY_URL)
+                    push_to_gateway(THOTH_METRICS_PUSHGATEWAY_URL, job="cleanup", registry=_PROMETHEUS_REGISTRY)
+                except Exception as exc:
+                    _LOGGER.exception("An error occurred pushing the metrics: %s", exc)
 
-            _delete_outdated_jobs(jobs)
-        except NotFoundException:
-            pass
 
-    if THOTH_METRICS_PUSHGATEWAY_URL:
-        try:
-            _LOGGER.debug(
-                "Submitting metrics to Prometheus pushgateway %r",
-                THOTH_METRICS_PUSHGATEWAY_URL,
-            )
-            push_to_gateway(
-                THOTH_METRICS_PUSHGATEWAY_URL,
-                job="cleanup",
-                registry=prometheus_registry,
-            )
-        except Exception as e:
-            _LOGGER.exception("An error occurred pushing the metrics: {!r}".format(e))
+if __name__ == "__main__":
+    sys.exit(cli())
